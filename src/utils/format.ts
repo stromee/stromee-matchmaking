34704fa21f203ee09a07f91757d3f89028ce955c
David Röger <david@codeatelier.com>
2023-11-27 12:55:06 +0100

    Add format util

---
+++ src/utils/format.ts
@@ -0,0 +1,19 @@
+const formatNumber = (number: number | string) => {
+	if (typeof number === 'number') {
+		return formatNumber(number.toString());
+	}
+
+	return number.replace('.', ',');
+};
+
+export const formatDistance = (distance: number) => {
+	if (distance < 1) {
+		return formatNumber(distance.toFixed(1));
+	}
+
+	return formatNumber(distance.toFixed());
+};
+
+export const formatUnit = (value: string, unit: string) => {
+	return `${value} ${unit}`;
+};
