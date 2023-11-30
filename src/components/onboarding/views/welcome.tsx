e58f90b2504fc290e7a314346338dee4b0d07239
David Röger <david@codeatelier.com>
2023-11-30 17:37:45 +0100

    Add scrollview to welcome

--- src/components/onboarding/views/welcome.tsx
+++ src/components/onboarding/views/welcome.tsx
@@ -1,4 +1,4 @@
-import { H1, H2, Image, View, YStack } from 'tamagui';
+import { H1, H2, Image, ScrollView, View, YStack } from 'tamagui';
 
 import { Button } from '@components/themed/button';
 
@@ -8,25 +8,32 @@ type WelcomeProps = OnboardingCarouselProps;
 
 const Welcome = ({ onNext: handleNext }: WelcomeProps) => {
 	return (
-		<YStack flex={1} px="$4" py="$8" gap="$4" jc="space-between">
-			<H1 alignSelf="center">Stromee</H1>
-			<Image
-				width={300}
-				height={300}
-				resizeMode="contain"
-				alignSelf="center"
-				source={{
-					uri: '/images/infogr_1_producer.png',
-				}}
-			/>
-			<View px="$8">
-				<H2 textAlign="center">
-					Ab ins Energieabendteuer - <br />
-					Swipe dir deinen passenden Produzenten!
-				</H2>
-			</View>
-			<Button onPress={handleNext}>Los gehts!</Button>
-		</YStack>
+		<ScrollView
+			flex={1}
+			minHeight="$full"
+			contentContainerStyle={{ flex: 1, minHeight: '100%' }}
+		>
+			<YStack flex={1} px="$4" py="$8" gap="$4" jc="space-between">
+				<H1 alignSelf="center">Stromee</H1>
+				<Image
+					maxWidth="300px"
+					width="$full"
+					aspectRatio="1/1"
+					resizeMode="contain"
+					alignSelf="center"
+					source={{
+						uri: '/images/infogr_1_producer.png',
+					}}
+				/>
+				<View px="$8">
+					<H2 textAlign="center">
+						Ab ins Energieabenteuer - <br />
+						Swipe dir deinen passenden Produzenten!
+					</H2>
+				</View>
+				<Button onPress={handleNext}>Los gehts!</Button>
+			</YStack>
+		</ScrollView>
 	);
 };
 
